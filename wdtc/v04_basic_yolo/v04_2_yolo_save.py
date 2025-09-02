3c0c04493c67ace85d39325d0f7d998576613aba
murbachovski <murbachovski@gmail.com>
2025-09-02 18:08:39 +0900

    Delete wdtc directory

--- wdtc/v04_basic_yolo/v04_2_yolo_save.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# 1. 모델 로드
-model = YOLO("yolo11n.pt")
-
-# 2. 모델 예측
-results = model("v04_basic_yolo/input.jpg", save=True)
--- wdtc/v06_data_aug/v06_1_pill.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image, ImageEnhance
-import matplotlib.pyplot as plt
-
-# 1. 이미지 로드
-# 2. 이미지 조정
-# 3. 결과 시각화
-# 4. 결과 이미지 저장
